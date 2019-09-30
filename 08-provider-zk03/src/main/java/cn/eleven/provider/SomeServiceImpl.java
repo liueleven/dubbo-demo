d43f0d0435c8e77ef6ffb6aeb6ed4a66a4c84d26
liueleven <liueleven@aliyun.com>
2019-09-30 16:49:58 +0800

    负载均衡

---
+++ 08-provider-zk03/src/main/java/cn/eleven/provider/SomeServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.eleven.provider;
+
+
+import cn.eleven.api.SomeService;
+
+public class SomeServiceImpl implements SomeService {
+    @Override
+    public String hello(String name) {
+        System.out.println(name + "，第三个服务提供者");
+        return "第三个服务提供者";
+    }
+
+}
